80d1e830dd972de0ff0266d3ed21cf1aceafb007
Zicio <zicio@yandex.ru>
2022-11-18 15:26:23 +0400

    Worked on LoginPage

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,4 +1,19 @@
+import { NavLink } from "react-router-dom";
+
 const Navbar = () => {
-  return <div>Navbar</div>;
+  const user = true;
+  return (
+    <div className="w-full bg-green-500 h-[60px] p-[5px] text-xl flex items-center">
+      <div className="flex justify-end items-center mr-[50px] w-full">
+        {user ? (
+          <NavLink to={process.env.REACT_APP_LOGIN_ROUTE!}>
+            <button className="button">выйти</button>
+          </NavLink>
+        ) : (
+          <button className="button">войти</button>
+        )}
+      </div>
+    </div>
+  );
 };
 export default Navbar;
--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,4 +1,10 @@
 const LoginPage = () => {
-  return <div>LoginPage</div>;
+  return (
+    <div className="flex items-center justify-center h-[calc(100vh-60px)]">
+      <div className="p-[20px] w-[300px] h-[100px] bg-slate-400 flex flex-col justify-center ">
+        <button className="button w-[250px]">Войти с помощью Google</button>
+      </div>
+    </div>
+  );
 };
 export default LoginPage;
